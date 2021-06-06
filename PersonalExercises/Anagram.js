63097844c134aefc445a20f4fc88e890ce5c35da
Rodrigo <cuitlagua@gmail.com>
2021-06-05 20:36:51 -0500

    fix: correction in Valid Anagram Function

--- PersonalExercises/Anagram.js
+++ PersonalExercises/Anagram.js
@@ -3,29 +3,29 @@ Given two strings, write a function to determine if the second string is an anag
 An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
 */
 
-
 function validAnagram(wordA, wordB) {
-  if(wordA.length !== wordB.length) return false;
-  objA = {}
-  objB = {}
-  for (const word of wordA) {
-    objA[word] = (objA[word] || 0) + 1 
-  }
-  for (const word of wordB) {
-    objB[word] = (objB[word] || 0) + 1
-  }
+	// add whatever parameters you deem necessary - good luck!
+	if (wordA.length !== wordB.length) return false;
+	const objA = {};
+	const objB = {};
+	for (const word of wordA) {
+		objA[word] = (objA[word] || 0) + 1;
+	}
+	for (const word of wordB) {
+		objB[word] = (objB[word] || 0) + 1;
+	}
 
-  for(let key in objA){
-    // If the key exist
-    if(!(key in objB)) return false;
+	for (let key in objA) {
+		// If the key exist
+		if (!(key in objB)) return false;
 
-    if(objA[key] !== objB[key]) return false;
-  }
+		if (objA[key] !== objB[key]) return false;
+	}
 
-  return true;
+	return true;
 }
 
-const firstWd = 'anagram'
-const secondWd = 'nagaram'
+const firstWd = 'anagram';
+const secondWd = 'nagaram';
 
 console.log(validAnagram(firstWd, secondWd));
