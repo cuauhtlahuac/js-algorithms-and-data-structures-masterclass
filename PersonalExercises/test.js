f8e849e299b77e329c736251bd9ffb8d0b43bea2
Rodrigo <cuitlagua@gmail.com>
2021-06-07 23:08:46 -0500

    Personal: Some Js Objects creators tests

--- PersonalExercises/test.js
+++ PersonalExercises/test.js
@@ -1,1 +1,20 @@
 console.log('test');
+
+function Person(name){
+  var object = {};
+  object.name=name;
+  object.age=21;
+  return object;
+}
+var object = new Person("Sudheer");
+var tcejbo = new Person("Madre mía");
+
+console.log(object)
+console.log(tcejbo)
+
+const singleton = new function(){
+  this.name = "Sudheer";
+}
+singleton.name = 'hola' 
+
+console.log(singleton);
