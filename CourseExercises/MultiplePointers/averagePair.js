13a435a6c3d145241b953a7e28a09ffe193fa820
Rodrigo <cuitlagua@gmail.com>
2021-06-24 23:19:29 -0500

    JAM: averagePair solved

--- CourseExercises/MultiplePointers/averagePair.js
+++ CourseExercises/MultiplePointers/averagePair.js
@@ -4,25 +4,26 @@
  * There may be more than one pair that matches the average target.
  */
 
+function averagePair(arr, avg) {
+	// add whatever parameters you deem necessary - good luck!
+	if (arr.length < 1) return false;
 
-function averagePair(arr, avg){
-  // add whatever parameters you deem necessary - good luck!
-  if(arr.length < 1) return false;
+	let start = 0;
+	let end = 0;
+	let base = 2 * avg;
 
-  let start = 0;
-  let end = 0;
-  let base = 2 * avg;
-  console.log(base);
-
-  for (let i = 0; i < arr.length; i++) {
-    const element = arr[i];
-    if(base - element === arr[i +  1] ) console.log("We found");
-    
-  }
-  console.log("Done");
+	for (let i = 0; i < arr.length; i++) {
+		const element = arr[i];
+		const current = base - element;
+		for (let j = 1; j < arr.length; j++) {
+			const element = arr[j];
+			if (current === element) return true;
+		}
+	}
+	return false;
 }
 
-averagePair([1,2,3],2.5) // true
-averagePair([1,3,3,5,6,7,10,12,19],8) // true
-averagePair([-1,0,3,4,5,6],4.1) // false
-averagePair([],4) // false
+console.log(averagePair([ 1, 2, 3 ], 2.5)); // true
+console.log(averagePair([ 1, 3, 3, 5, 6, 7, 10, 12, 19 ], 8)); // true
+console.log(averagePair([ -1, 0, 3, 4, 5, 6 ], 4.1)); // false
+console.log(averagePair([], 4)); // false
---
+++ CourseExercises/MultiplePointers/averagePairTeacher.js
@@ -0,0 +1,16 @@
+function averagePair(arr, num){
+  let start = 0
+  let end = arr.length-1;
+  while(start < end){
+    let avg = (arr[start]+arr[end]) / 2 
+    if(avg === num) return true;
+    else if(avg < num) start++
+    else end--
+  }
+  return false;
+}
+
+console.log(averagePair([ 1, 2, 3 ], 2.5)); // true
+console.log(averagePair([ 1, 3, 3, 5, 6, 7, 10, 12, 19 ], 8)); // true
+console.log(averagePair([ -1, 0, 3, 4, 5, 6 ], 4.1)); // false
+console.log(averagePair([], 4)); // false
