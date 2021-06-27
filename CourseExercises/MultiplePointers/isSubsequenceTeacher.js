4f4f05b0d7d6eead7525b2d8bdfd8f994cb132e6
Rodrigo <cuitlagua@gmail.com>
2021-06-26 21:17:04 -0500

    JAM: isSubsequent teacher solution

---
+++ CourseExercises/MultiplePointers/isSubsequenceTeacher.js
@@ -0,0 +1,31 @@
+function isSubsequence(str1, str2) {
+  var i = 0;
+  var j = 0;
+  if (!str1) return true;
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) i++;
+    if (i === str1.length) return true;
+    j++;
+  }
+  return false;
+}
+
+/*
+
+isSubsequence Solution - Recursive but not O(1)
+
+function isSubsequence(str1, str2) {
+  if(str1.length === 0) return true
+  if(str2.length === 0) return false
+  if(str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))  
+  return isSubsequence(str1, str2.slice(1))
+}
+
+*/
+
+
+console.log(isSubsequence('hello', 'hello world')); // true
+console.log(isSubsequence('sing', 'sting')); // true
+console.log(isSubsequence('abc', 'abracadabra')); // true
+console.log(isSubsequence('circulo de sol', 'reciporcupllo palde lopsoyrl')); // true
+console.log(isSubsequence('abc', 'acb')); // false (order matters)
