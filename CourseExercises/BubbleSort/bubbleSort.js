6fdf4d47a92cae2e2d0cc2cd934c5d02a208a05c
Rodrigo <cuitlagua@gmail.com>
2021-07-23 18:08:42 -0500

    JAM: Optimized bubble sort, (doesn't hit undefine)

--- CourseExercises/BubbleSort/bubbleSort.js
+++ CourseExercises/BubbleSort/bubbleSort.js
@@ -1,8 +1,8 @@
 //Start looping from with a variable called i the end of the array towards the beginning
 function bubbleSort(arr) {
-	for (let i = 0; i < arr.length; i++) {
+	for (let i = arr.length; i > 0; i--) {
 		//Start an inner loop with a variable called j from the beginning until i - 1
-		for (let j = 0; j < arr.length; j++) {
+		for (let j = 0; j < i - 1; j++) {
 			//If arr[j] is greater than arr[j+1], swap those two values!
 			if (arr[j] > arr[j + 1]) {
 				let temp = arr[j + 1];
