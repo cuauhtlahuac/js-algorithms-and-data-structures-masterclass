caf8ba021cb77c7f4c9a200cc68065e04fc2349c
Rodrigo <cuitlagua@gmail.com>
2021-08-12 22:17:20 -0500

    JAM: Merge sort first try and horrible fail

--- CourseExercises/MergeSort/mergeSort.js
+++ CourseExercises/MergeSort/mergeSort.js
@@ -2,28 +2,26 @@ const mergingArrays = require('./mergingArrays');
 
 function mergeSort(array) {
 	// * Break up the array into halves until you have arrays that are empty or have one element
-	function breakArrayHelper(array) {
-		const copiedArrayFirst = array.slice(0, array.length / 2);
-		const copiedArraySecond = array.slice(array.length / 2, array.length);
-
-		console.log({ copiedArrayFirst, copiedArraySecond });
-		function breakRecursive(first, second) {
-			if (first && first.length < 2) {
-				return first;
-			} else if(second && second.length < 2){
-				return second;
-			}
-			
-			const current1 = first.slice(0,  first.length / 2);
-			const current2 = second.slice(second.length / 2,  second.length);
-			console.log({ first, current1, second, current2 });
-			return breakRecursive(current1, current2)
-
+	const copiedArrayFirst = array.slice(0, array.length / 2);
+	const copiedArraySecond = array.slice(array.length / 2, array.length);
+	function breakArrayHelper(arr1, arr2) {
+		if(!arr1 || !arr1.length || arr1.length < 2){
+			return arr1;
 		}
-		return breakRecursive(array);
+		if(!arr2 || !arr2.length || arr2.length < 2){
+			return arr2;
+		}
+		const nextArr1 = arr1.slice(0, arr1.length / 2)
+		const nextArr2 = arr2.slice(arr2.length / 2, arr2.length)
+		//const l = mergingArrays(nextArr2, nextArr2)
+		console.log({arr1,  arr2, });
+		 return breakArrayHelper(nextArr1, nextArr2);
 	}
+
+	breakArrayHelper(copiedArrayFirst, copiedArraySecond)
+
+
 	// * Once you have smaller sorted arrays, merge those arrays with other sorted arrays until you are back at the full length of the array
-	return breakArrayHelper(array);
 	// * Once the array has been merged back together, return the merged (and sorted!) array
 }
 
