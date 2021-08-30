e075d258994426a4e3addaa925ee0ec16d68e199
Cuitlahuac Rodrigo Aristeo Campos <cuitlagua@gmail.com>
2021-08-29 20:20:49 -0500

    JAM: extra notes about pivot

--- CourseExercises/QuickSort/pivot.js
+++ CourseExercises/QuickSort/pivot.js
@@ -9,15 +9,19 @@ function pivot(arr, start=0, end=arr.length+1){
   var pivot = arr[start];
   // Store the current pivot index in a variable (this will keep track of where the pivot should end up)
   var swapIdx = start;
-// Loop through the array from the start until the end
+// Loop through the array from the first element, after pivot (start + 1), until the end
   for(var i = start + 1; i < arr.length; i++){
-    // If the pivot is greater than the current element, increment the pivot index variable and then swap the current element with the element at the pivot index
+    // If the pivot is greater than the current element (witch is as we said before the next element after pivot),
+    // increment the pivot index variable and then swap the current element with the element at the pivot index
     if(pivot > arr[i]){
+      // next line say that at least one element is less than the pivot and now we can keep track of how many elements are less than pivot
       swapIdx++;
+      // Here we need to swap the element 
       swap(arr,swapIdx,i);
     }
   }
   // Swap the starting element (i.e. the pivot) with the pivot index
+  // This means that at the final we swap the current swapIdx with the pivot, because the value "start" never changed
   swap(arr,start,swapIdx);
   console.log({arr});
   // Return the pivot index
