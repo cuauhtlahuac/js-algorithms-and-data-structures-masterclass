bdabb8998cb7294b8674843a3aa79df44b179dce
Cuitlahuac Rodrigo Aristeo Campos <cuitlagua@gmail.com>
2021-09-10 22:52:42 -0500

    fix: radix receives an a array instead a number

--- CourseExercises/RadixSort/radixSort.js
+++ CourseExercises/RadixSort/radixSort.js
@@ -1,22 +1,24 @@
 const Radix = require("./radixSortClass");
 
 // - Define a function that accepts list of numbers
-function radixSort(num){
-  const radix = new Radix(num);
-// - Figure out how many digits the largest number has
+function radixSort(list){
+  const radix = new Radix();
+  // - Figure out how many digits the largest number has
 const numLength = radix.countDigits();
 // - Loop from k = 0 up to this largest number of digits
 for (let k = 0; k < numLength; k++) {
   // - For each iteration of the loop:
   // - Create buckets for each digit (0 to 9)
+  const bucket = []
   // - place each number in the corresponding bucket based on its kth digit
-  console.log(k);
+  
   
 }
 
 // - Replace our existing array with values in our buckets, starting with 0 and going up to 9
+
 // - return list at the end!
-  return num
+  return list;
 }
 
-console.log(radixSort(345));
+console.log(radixSort([2,4,667,897,234,12,3,6,345]));
