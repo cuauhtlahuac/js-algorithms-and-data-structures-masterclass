c1fa8822a9b214002b58cc6fd3a7739fdc61cccc
Cuitlahuac Rodrigo Aristeo Campos <cuitlagua@gmail.com>
2021-09-15 20:09:56 -0500

    JAM: Save each number in its bucket

--- CourseExercises/RadixSort/radixSort.js
+++ CourseExercises/RadixSort/radixSort.js
@@ -1,33 +1,32 @@
-const Radix = require("./radixSortClass");
+const Radix = require('./radixSortClass');
 
 // - Define a function that accepts list of numbers
-function radixSort(list){
-  const radix = new Radix(list);
-  // - Figure out how many digits the largest number has
-  radix.mostDigitNumbers();
-// - Loop from k = 0 up to this largest number of digits
-
-// - Create buckets for each digit (0 to 9)
-  let buckets = radix.createBuckets(0, 9);
+function radixSort(list) {
+	const radix = new Radix(list);
+	// - Figure out how many digits the largest number has
+	radix.mostDigitNumbers();
   
-for (let k = 0 + 1; k < radix.maxDigitNumber; k++) {
-  // - For each iteration of the loop:
-  // - place each number in the corresponding bucket based on its kth digit
+	// - Loop from k = 0 up to this largest number of digits
+	for (let k = 0; k < radix.maxDigitNumber; k++) {
+    // - For each iteration of the loop:
+    // - Create buckets for each digit (0 to 9)
+    let buckets = radix.createBuckets(0, 10);
+		// - place each number in the corresponding bucket based on its kth digit
+    for (let i = 0; i < list.length; i++) {
+      const number = list[i];
+      
+      const kthDigit = radix.getDigitPositionTeacherMethod(number,k);
 
-  /*
-  if(bucket[k]){
-    bucket[k].push(k)
-  }else{
-    bucket[k] = [k]
-  }
-  console.log({k,bucket});
+      buckets[kthDigit].push(number);
+    }
+    
+		// - Replace our existing array with values in our buckets, starting with 0 and going up to 9
+    
+    console.log({ k, buckets, number: list[k], max: radix.maxDigitNumber });
+	}
+
+		// - return list at the end!
+		return list;
 }
-*/
 
-// - Replace our existing array with values in our buckets, starting with 0 and going up to 9
-
-// - return list at the end!
-  return list;
-}
-
-console.log(radixSort([2,4,667,897,234,12,3,6,345, 98789457]));
+console.log(radixSort([ 20, 41, 6673, 897, 234, 12, 38, 69, 345, 98789456, 25 ]));
