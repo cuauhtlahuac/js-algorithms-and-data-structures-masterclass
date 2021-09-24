4ae048bdc0c680704f7f6951fabb8ab4a048b18a
Cuitlahuac Rodrigo Aristeo Campos <cuitlagua@gmail.com>
2021-09-23 22:21:23 -0500

    JAM: SinglyLinkedList - Solution following pseudo

--- CourseExercises/LinkedList/singlyLinkedList.js
+++ CourseExercises/LinkedList/singlyLinkedList.js
@@ -15,13 +15,23 @@ class SinglyLinkedList {
 		this.length = 0;
 	}
 
+	// This function should accept a value
 	push(val) {
-		// This function should accept a value
 		// Create a new node using the value passed to the function
+		const node = new Node(val)
 		// If there is no head property on the list, set the head and tail to be the newly created node
-		// Otherwise set the next property on the tail to be the new node and set the tail property on the list to be the newly created node
+		if(!this.tail && !this.head){
+			this.tail = node;
+			this.head = node;
+		} else{
+			// Otherwise set the next property on the tail to be the new node and set the tail property on the list to be the newly created node
+			this.tail.next = this.tail;
+			this.tail = node;
+		}
 		// Increment the length by one
+		this.length++
 		// Return the linked list
+		return SinglyLinkedList;
 	}
 }
 
