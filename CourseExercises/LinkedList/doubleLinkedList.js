b9fc627347408ee0ae227200851353d551379018
Rodrigo macos sierra <cuitlagua@gmail.com>
2021-10-23 20:40:15 -0500

    JAM: Double linked list - basic structure

---
+++ CourseExercises/LinkedList/doubleLinkedList.js
@@ -0,0 +1,23 @@
+// NODE
+class Node {
+	constructor(val) {
+		// - val
+		this.val;
+		// - next
+		this.next = null;
+		// - prev
+		this.prev = null;
+	}
+}
+
+// Doubly Linked List
+class DoublyLinkedList {
+	constructor() {
+		// - head
+		this.head = null;
+		// - tail
+		this.tail = null;
+		// - length
+		this.length = 0;
+	}
+}
